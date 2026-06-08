79da9a1fdce018b00e17f69a51544b22a14283c5
Chizzy312 <chizzytony111@gmail.com>
2026-06-08 14:49:12 +0100

    python projects

---
+++ Chizzy's file.py
@@ -0,0 +1,11 @@
+print("hello world")
+book1 = int(input("Enter a number: "))
+book2 = int(input("Enter another "))
+add = book1 + book2
+subtract = book1 - book2
+divide = book1 / book2
+multiply = book1 * book2
+print(add)
+print(subtract)
+print(divide)
+print(multiply)
